209557a3335233898f007feb9e6945bb1fa081ac
hiro <hiro0218@gmail.com>
2016-08-23 20:34:40 +0900

    :innocent: add exe information

--- config.js
+++ config.js
@@ -19,7 +19,20 @@ let config = {
   // electron-packager options
   // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
   building: {
+    // darwin
     'app-version': pkg.version,
+    'app-bundle-id': 'jp.0218.app.Miikun',
+    // win32
+    'version-string': {
+      FileDescription: pkg.description,
+      FileVersion: pkg.version,
+      ProductName: pkg.name,
+      ProductVersion: pkg.version,
+      CompanyName: pkg.author,
+      OriginalFilename: pkg.name,
+      InternalName: pkg.name,
+      LegalCopyright: pkg.copyright
+    },
     arch: 'x64',
     asar: true,
     dir: path.join(__dirname, 'app'),
