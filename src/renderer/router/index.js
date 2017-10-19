1e6e37895d46e3bd499f74da3c9d9f4322ce818d
hiro <hiro0218@gmail.com>
2017-10-19 14:07:01 +0900

    update: packages
    > [Vue warn]: Failed to mount component: template or render function not defined.
    Vue warnの解消

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'mii-main',
-      component: require('@/components/Main')
+      component: require('@/components/Main').default
     },
     {
       path: '*',
