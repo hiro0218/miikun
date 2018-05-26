3ec85391bab7a2d91dd92cfe39588acee23962c3
ZaneTs <darg20127@gmail.com>
2018-05-27 00:58:45 +0800

    add Errors.js

---
+++ src/renderer/modules/Errors.js
@@ -0,0 +1,36 @@
+const ERR_USER_CANCEL = -100
+const ERR_ENCRYPT_FAIL = -101
+const ERR_DECRYPT_FAIL = -102
+
+class UserCancelError extends Error {
+  constructor () {
+    super('User Canceled.')
+    this.name = 'UserCancelError'
+    this.code = ERR_USER_CANCEL
+  }
+}
+
+class EncryptFailError extends Error {
+  constructor (reason) {
+    super(reason)
+    this.name = 'EncryptFailError'
+    this.code = ERR_ENCRYPT_FAIL
+  }
+}
+
+class DecryptFailError extends Error {
+  constructor (reason) {
+    super(reason)
+    this.name = 'DecryptFailError'
+    this.code = ERR_DECRYPT_FAIL
+  }
+}
+
+export {
+  ERR_USER_CANCEL,
+  UserCancelError,
+  ERR_ENCRYPT_FAIL,
+  EncryptFailError,
+  ERR_DECRYPT_FAIL,
+  DecryptFailError
+}
