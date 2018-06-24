3d3975a4a0261b78e1dc392c83c95d1d155f3242
Zanets <darg20127@gmail.com>
2018-06-24 14:47:50 +0800

    rewrite with class

--- src/renderer/modules/Encryptor.js
+++ src/renderer/modules/Encryptor.js
@@ -3,58 +3,59 @@
 import crypto from 'crypto';
 import { DecryptFailError } from './Errors';
 
-function Encryptor() {
-  this.info = {
-    cipher: {
-      algorithm: 'aes-256-cbc',
-      ivLength: 16,
-    },
-    hmac: {
-      algorithm: 'sha256',
-      key: 'sjIIhOQJWjlOl4J',
-    },
-  };
-}
-
-Encryptor.prototype.getInfo = function() {
-  return this.info;
-};
-
-// return: Object { iv, enc content }
-Encryptor.prototype.encrypt = function(key, raw) {
-  let iv = crypto.randomBytes(this.info.cipher.ivLength);
-  let cipher = crypto.createCipheriv(this.info.cipher.algorithm, this.hmac(key), iv);
-  return {
-    iv: iv,
-    result: Buffer.concat([cipher.update(raw), cipher.final()]),
-  };
-};
-
-// return: Buffer
-Encryptor.prototype.decrypt = function(key, raw, iv) {
-  let decipher = crypto.createDecipheriv(this.info.cipher.algorithm, this.hmac(key), Buffer.from(iv));
-  try {
-    return Buffer.concat([decipher.update(raw), decipher.final()]);
-  } catch (err) {
-    throw new DecryptFailError('Wrong Password');
+class Encryptor {
+  constructor() {
+    this.info = {
+      cipher: {
+        algorithm: 'aes-256-cbc',
+        ivLength: 16,
+      },
+      hmac: {
+        algorithm: 'sha256',
+        key: 'sjIIhOQJWjlOl4J',
+      },
+    };
   }
-};
 
-// return: Buffer
-Encryptor.prototype.hmac = function(data) {
-  let hmac = crypto.createHmac(this.info.hmac.algorithm, this.info.hmac.key);
-  hmac.update(data);
-  return hmac.digest();
-};
+  getInfo() {
+    return this.info;
+  }
 
-// return: Array[String]
-Encryptor.prototype.listCiphers = function() {
-  return crypto.getCiphers();
-};
+  // return: Object { iv, enc content }
+  encrypt(key, content) {
+    let iv = crypto.randomBytes(this.info.cipher.ivLength);
+    let cipher = crypto.createCipheriv(this.info.cipher.algorithm, this.hmac(key), iv);
+    return {
+      iv: iv,
+      content: Buffer.concat([cipher.update(content), cipher.final()]),
+    };
+  }
 
-// return: Array[String]
-Encryptor.prototype.listHashes = function() {
-  return crypto.getHashes();
-};
+  // return: Buffer
+  decrypt(key, raw, iv) {
+    let decipher = crypto.createDecipheriv(this.info.cipher.algorithm, this.hmac(key), Buffer.from(iv));
+    try {
+      return Buffer.concat([decipher.update(raw), decipher.final()]);
+    } catch (err) {
+      throw new DecryptFailError('Wrong Password');
+    }
+  }
+
+  // return: Buffer
+  hmac(data) {
+    let hmac = crypto.createHmac(this.info.hmac.algorithm, this.info.hmac.key);
+    hmac.update(data);
+    return hmac.digest();
+  }
+
+  // return: Array[String]
+  listCiphers() {
+    return crypto.getCiphers();
+  }
+
+  listHashes() {
+    return crypto.getHashes();
+  }
+}
 
 export default new Encryptor();
--- src/renderer/modules/Filesystem.js
+++ src/renderer/modules/Filesystem.js
@@ -8,148 +8,143 @@ import store from '../store';
 // This file looks looks
 // | Base info | HMAC | IV | Enc Content |
 // | 16        | 32   | 16 | ..          |
-function Filesystem() {
-  this.header = {
-    length: 64,
-    baseLength: 16,
-    hmacLength: 32,
-    ivLength: 16,
-  };
-  this.key = null;
-}
 
-// Return: Object contains header metadata
-Filesystem.prototype.getHeader = function() {
-  return this.header;
-};
-
-// Return: Boolean
-Filesystem.prototype.shouldEncrypt = function(path) {
-  return path.endsWith('.mii');
-};
-
-Filesystem.prototype.writeFile = function(path, content, cb, key = null) {
-  if (this.shouldEncrypt(path)) {
-    // Use the cached key instead of state, because when readFile fail, I
-    // can keep the old key of current file.
-    // It's hard and will make code looks ugly if implemnt in Editor.vue.
-    key = key === null ? this.key : key;
-
-    // Prevent null key, it should not happen
-    // at this time.
-    if (key === '' || key === null) {
-      cb(new NullKeyError());
-      return;
-    }
-
-    // Backup file at develop.
-    if (process.env.NODE_ENV === 'development') {
-      fs.writeFile(path + '.backup.md', content, 'utf8', cb);
-    }
-
-    content = this.encrypt(key, content);
+class Filesystem {
+  constructor() {
+    this.header = {
+      length: 64,
+      baseLength: 16,
+      hmacLength: 32,
+      ivLength: 16,
+    };
+    this.key = null;
   }
 
-  fs.writeFile(path, content, 'utf8', cb);
-};
-
-Filesystem.prototype.readFile = function(path, cb) {
-  let key = null;
-  const isEncrypt = this.shouldEncrypt(path);
-
-  if (isEncrypt) {
-    key = store.state.Editor.crypt.key;
-
-    // Prevent null key, it should not happen
-    // at this time.
-    if (key === '' || key === null) {
-      cb(new NullKeyError());
-      return;
-    }
+  // Return: Object contains header metadata
+  getHeader() {
+    return this.header;
   }
 
-  // Notice it's an async function, only return
-  // the callback, not readFile
-  fs.readFile(path, (err, content) => {
-    if (err) {
-      cb(err, null);
-      return;
-    }
+  // Return: Boolean
+  shouldEncrypt(path) {
+    return path.endsWith('.mii');
+  }
 
-    // Decrypt
+  writeFile(path, content, cb, key = null) {
+    if (this.shouldEncrypt(path)) {
+      // Use the cached key instead of state, because when readFile fail, I
+      // can keep the old key of current file.
+      // It's hard and will make code looks ugly if implemnt in Editor.vue.
+      key = key === null ? this.key : key;
+      // Prevent null key, it should not happen
+      // at this time.
+      if (key === '' || key === null) {
+        cb(new NullKeyError());
+        return;
+      }
+      // Backup file at develop.
+      if (process.env.NODE_ENV === 'development') {
+        fs.writeFile(path + '.backup.md', content, 'utf8', cb);
+      }
+      content = this.encrypt(key, content);
+    }
+    fs.writeFile(path, content, 'utf8', cb);
+  }
+
+  readFile(path, cb) {
+    let key = null;
+    const isEncrypt = this.shouldEncrypt(path);
+
     if (isEncrypt) {
-      try {
-        content = this.decrypt(key, content);
-      } catch (err2) {
-        cb(err2, null);
+      key = store.state.Editor.crypt.key;
+      // Prevent null key, it should not happen
+      // at this time.
+      if (key === '' || key === null) {
+        cb(new NullKeyError());
         return;
       }
     }
-
-    this.updateKey(key);
-    cb(null, content.toString('utf8'));
-  });
-};
-
-Filesystem.prototype.encrypt = function(key, content) {
-  // This return { iv, result }
-  let encResult = encryptor.encrypt(key, content);
-  let bufHmac = encryptor.hmac(content);
-  // Pack base info, hmac of origin content, iv, enc content
-  let bufFile = this.packHeader(encResult, bufHmac);
-  return bufFile;
-};
-
-Filesystem.prototype.decrypt = function(key, content) {
-  let fileStruct = this.unpackHeader(content);
-  try {
-    let decContent = encryptor.decrypt(key, fileStruct.encContent, fileStruct.iv);
-    if (encryptor.hmac(decContent).equals(fileStruct.hmac)) {
-      return decContent;
-    } else {
-      throw new DecryptFailError('Content mismatch.');
-    }
-  } catch (err) {
-    throw err;
+    // Notice it's an async function, only return
+    // the callback, not readFile
+    fs.readFile(path, (err, content) => {
+      if (err) {
+        cb(err, null);
+        return;
+      }
+      // Decrypt
+      if (isEncrypt) {
+        try {
+          content = this.decrypt(key, content);
+        } catch (err2) {
+          cb(err2, null);
+          return;
+        }
+      }
+      this.updateKey(key);
+      cb(null, content.toString('utf8'));
+    });
   }
-};
 
-// Return: Buffer
-Filesystem.prototype.packHeader = function(encResult, bufHmac) {
-  const info = this.getHeader();
-  let bufHeader = Buffer.alloc(info.length);
-  // * Base info, add whatever you want :P
-  bufHeader.write('Miikun@2018');
-  // * Encrypt info
-  bufHmac.copy(bufHeader, info.baseLength);
-  // * IV
-  encResult.iv.copy(bufHeader, info.baseLength + info.hmacLength);
-  // this.dumpHeader(bufHeader)
-  return Buffer.concat([bufHeader, encResult.result]);
-};
+  encrypt(key, content) {
+    // This return { iv, content }
+    let encResult = encryptor.encrypt(key, content);
+    let bufHmac = encryptor.hmac(content);
+    // Pack base info, hmac of origin content, iv, enc content
+    let bufFile = this.packHeader(encResult, bufHmac);
+    return bufFile;
+  }
 
-Filesystem.prototype.unpackHeader = function(raw) {
-  const info = this.getHeader();
-  let base = raw.slice(0, info.baseLength);
-  let hmac = raw.slice(info.baseLength, info.baseLength + info.hmacLength);
-  let iv = raw.slice(info.baseLength + info.hmacLength, info.length);
-  let encContent = raw.slice(info.length);
-  return { base, hmac, iv, encContent };
-};
+  decrypt(key, content) {
+    let fileStruct = this.unpackHeader(content);
+    try {
+      let decContent = encryptor.decrypt(key, fileStruct.encContent, fileStruct.iv);
+      if (encryptor.hmac(decContent).equals(fileStruct.hmac)) {
+        return decContent;
+      } else {
+        throw new DecryptFailError('Content mismatch.');
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
 
-Filesystem.prototype.dumpHeader = function(header) {
-  const info = this.getHeader();
-  let base = header.slice(0, info.baseLength);
-  let hmac = header.slice(info.baseLength, info.baseLength + info.hmacLength);
-  let iv = header.slice(info.baseLength + info.hmacLength, info.length);
-  console.log('[Dump Header] Total bytes: ' + header.byteLength);
-  console.log('[Dump Header] Base info: ' + base.toString('utf8'));
-  console.log('[Dump Header] HMAC: ' + hmac.toString('utf8'));
-  console.log('[Dump Header] IV: ' + iv.toString('utf8'));
-};
+  // Return: Buffer
+  packHeader(encResult, bufHmac) {
+    const info = this.getHeader();
+    let bufHeader = Buffer.alloc(info.length);
+    // * Base info, add whatever you want :P
+    bufHeader.write('Miikun@2018');
+    // * Encrypt info
+    bufHmac.copy(bufHeader, info.baseLength);
+    // * IV
+    encResult.iv.copy(bufHeader, info.baseLength + info.hmacLength);
+    // this.dumpHeader(bufHeader)
+    return Buffer.concat([bufHeader, encResult.content]);
+  }
 
-Filesystem.prototype.updateKey = function(key) {
-  this.key = key;
-};
+  unpackHeader(raw) {
+    const info = this.getHeader();
+    let base = raw.slice(0, info.baseLength);
+    let hmac = raw.slice(info.baseLength, info.baseLength + info.hmacLength);
+    let iv = raw.slice(info.baseLength + info.hmacLength, info.length);
+    let encContent = raw.slice(info.length);
+    return { base, hmac, iv, encContent };
+  }
+
+  dumpHeader(header) {
+    const info = this.getHeader();
+    let base = header.slice(0, info.baseLength);
+    let hmac = header.slice(info.baseLength, info.baseLength + info.hmacLength);
+    let iv = header.slice(info.baseLength + info.hmacLength, info.length);
+    console.log('[Dump Header] Total bytes: ' + header.byteLength);
+    console.log('[Dump Header] Base info: ' + base.toString('utf8'));
+    console.log('[Dump Header] HMAC: ' + hmac.toString('utf8'));
+    console.log('[Dump Header] IV: ' + iv.toString('utf8'));
+  }
+
+  updateKey(key) {
+    this.key = key;
+  }
+}
 
 export default new Filesystem();
