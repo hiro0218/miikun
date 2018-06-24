4d47fcafa080c2c97a99bc85997c467d507bec9a
Zanets <darg20127@gmail.com>
2018-06-24 09:32:47 +0800

    add NullKeyError and fix style

--- src/renderer/modules/Errors.js
+++ src/renderer/modules/Errors.js
@@ -1,6 +1,7 @@
 const ERR_USER_CANCEL = -100;
 const ERR_ENCRYPT_FAIL = -101;
 const ERR_DECRYPT_FAIL = -102;
+const ERR_NULL_KEY = -103;
 
 /* Workaround of ReferenceError: _construct is not defined */
 const _Error = Error;
@@ -29,4 +30,21 @@ class DecryptFailError extends _Error {
   }
 }
 
-export { ERR_USER_CANCEL, UserCancelError, ERR_ENCRYPT_FAIL, EncryptFailError, ERR_DECRYPT_FAIL, DecryptFailError };
+class NullKeyError extends _Error {
+  constructor() {
+    super('Cannot find key.');
+    this.name = 'NullKeyError';
+    this.code = ERR_NULL_KEY;
+  }
+}
+
+export {
+  ERR_USER_CANCEL,
+  UserCancelError,
+  ERR_ENCRYPT_FAIL,
+  EncryptFailError,
+  ERR_DECRYPT_FAIL,
+  DecryptFailError,
+  ERR_NULL_KEY,
+  NullKeyError,
+};
