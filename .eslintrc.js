b7b9230338a8e98de969d45413a0bf31845c5d4f
hiro0218 <hiro0218@gmail.com>
2019-07-14 01:54:45 +0900

    fix: lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': [
       'error',
       {
