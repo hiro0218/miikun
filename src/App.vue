e0369c5d71172b8b73eb5575be8d00af306234d0
hiro0218 <hiro0218@gmail.com>
2019-07-16 00:54:20 +0900

    remove: unused import file

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,5 @@ export default {
 
 <style lang="scss">
 @import '~vue-material/dist/vue-material.min.css';
-@import '~vue-material/dist/theme/default.css';
 @import '@/assets/style/main.scss';
 </style>
