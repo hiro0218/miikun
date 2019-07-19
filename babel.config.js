b62a412dcaef855a4ddb41a4e17c8e7e77d1755a
hiro0218 <hiro0218@gmail.com>
2019-07-20 00:13:26 +0900

    chore: unity babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  presets: ['@vue/app'],
+module.exports = function(api) {
+  api.cache(true);
+  const presets = [
+    '@vue/app',
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        useBuiltIns: 'usage',
+      },
+    ],
+  ];
+  const plugins = ['@babel/plugin-transform-runtime'];
+  const comments = false;
+
+  return {
+    comments,
+    presets,
+    plugins,
+  };
 };
