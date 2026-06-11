96ea85d0215fb0e599b6c7c257733956a110df8e
hiro <hiro0218@gmail.com>
2026-06-11 20:39:50 +0900

    refactor: update Vue app bootstrap for Vue 3

--- src/lib/event-bus.js
+++ src/lib/event-bus.js
@@ -1,3 +1,12 @@
-import Vue from 'vue';
+const eventTarget = new EventTarget();
 
-export const EventBus = new Vue();
+export const EventBus = {
+  $on(name, handler) {
+    eventTarget.addEventListener(name, (event) => {
+      handler(...event.detail);
+    });
+  },
+  $emit(name, ...args) {
+    eventTarget.dispatchEvent(new CustomEvent(name, { detail: args }));
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from 'vuex';
 
 import modules from './modules';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default createStore({
   modules,
   strict: process.env.NODE_ENV !== 'production',
 });
